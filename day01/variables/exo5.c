a745d305c669401fc84f72659cb94dac5778e9be
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-09-02 14:27:24 +0100

    Add water state based on temperature

--- day01/variables/exo5.c
+++ day01/variables/exo5.c
@@ -1,1 +1,17 @@
-#include <stdio.h>
+
+int main() {
+    float celsius;
+
+    printf("Entrez la temperature en Celsius : ");
+    scanf("%f", &celsius);
+
+    if (celsius < 0) {
+        printf("L'eau est a l'etat solide.\n");
+    } else if (celsius >= 0 && celsius < 100) {
+        printf("L'eau est a l'etat liquide.\n");
+    } else {
+        printf("L'eau est a l'etat gazeux.\n");
+    }
+
+    return 0;
+}
