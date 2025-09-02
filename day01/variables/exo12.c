077010f61e7270524ffc54a0d02dc3b4b7e85090
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-09-02 14:41:02 +0100

    Add program to reverse a four-digit number

--- day01/variables/exo12.c
+++ day01/variables/exo12.c
@@ -6,14 +6,10 @@ int main() {
 
     printf("Entrez un nombre entier a quatre chiffres : ");
     scanf("%d", &nombre);
-
-    // Extraire chaque chiffre
     c1 = nombre / 1000;
     c2 = (nombre / 100) % 10;
     c3 = (nombre / 10) % 10;
     c4 = nombre % 10;
-
-    // Reconstruire le nombre inverse
     inverse = c4 * 1000 + c3 * 100 + c2 * 10 + c1;
 
     printf("Le nombre inverse est : %d\n", inverse);
