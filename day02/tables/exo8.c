308d6314369bedbe8ae6ad498ec4d926bf1946b8
lioubi arabi <l.loubi1886@uca.ac.ma>
2025-09-03 11:52:49 +0100

    copying an array and showing their both values

---
+++ day02/tables/exo8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("enter the number of your elements: ");
+    scanf("%d", &a);
+    int arr[a], arr2[a];
+    for(int i=0; i<a; i++){
+        printf("enter the element number %d: ",i+1);
+        scanf("%d", &arr[i]);
+    }
+    for(int j=0; j<a; j++){
+        arr2[j]=arr[j];
+    }
+    for(int k=0; k<a; k++){
+        printf("%d de arr et %d de arr2\n",arr[k] , arr2[k]);
+    }
+    return 0;
+}
