fbe9aae2d8831f1ea4f72e41f08753dd9d30d273
arabi loubi <l.loubi1886@uca.ac.ma>
2025-09-05 15:07:10 +0100

    functions pour trouver le max

---
+++ day02/functions/exo3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int max(int a, int b){
+    if(a<b){
+        return b;
+    }
+    else {
+        return a;
+    }
+}
+int main() {
+    int e1, e2;
+    printf("enter e1: ");
+    scanf("%d", &e1);
+    printf("enter e2: ");
+    scanf("%d", &e2);
+    printf("le max est: %d", max(e1, e2));
+    return 0;
+}
