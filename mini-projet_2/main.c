7821e75230837df90e2a1812b5a3e88f3493363e
arabi loubi <l.loubi1886@uca.ac.ma>
2025-09-06 22:55:18 +0100

    set up the dashboard

--- mini-projet_2/main.c
+++ mini-projet_2/main.c
@@ -4,9 +4,37 @@ struct contact {
     };
 
 int main() {
+    int choose;
     struct contact contacts[123];
     
-    
+    while(1){
+        printf("---dashboard---\n");
+        printf("1. add new contact\n");
+        printf("2. search a contact\n");
+        printf("3. modify a contact\n");
+        printf("4. delete a contact\n");
+        printf("5.show all\n\n");
+        printf("choose from the list: ");
+        scanf("%d", &choose);
+        switch(choose){
+            case 1:
+            
+            break;
+            case 2:
+            
+            break;
+            case 3:
+            
+            break;
+            case 4:
+            
+            break;
+            case 5:
+            
+            break;
+            default: printf("choose a correct number!!!\n");
+        }
+    }
     
     return 0;
 }
