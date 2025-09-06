2f17f565154b790046ae72153f592fad82317238
arabi loubi <l.loubi1886@uca.ac.ma>
2025-09-06 15:52:23 +0100

    calculate the power of a number

---
+++ day02/boucles2/exo9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("entrer un nombre: ");
+    scanf("%d", &a);
+    printf("entrer une puissance: ");
+    scanf("%d", &b);
+    b--;
+    while(b){
+        a *=a;
+        b--;
+    }
+    printf("resultas est %d", a);
+    return 0;
+}
