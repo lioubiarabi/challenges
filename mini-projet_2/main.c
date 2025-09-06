2cd34c569ae708447ed177389e2f266373036427
arabi loubi <l.loubi1886@uca.ac.ma>
2025-09-06 23:53:22 +0100

    create show function to display all the contacts

--- mini-projet_2/main.c
+++ mini-projet_2/main.c
@@ -6,11 +6,13 @@ void stringCopy(char *first, char *second){
     }
 }
 
-int n=0;
+int n=2;
 struct contact {
-        char name[50], email[50], number[50];
+        char name[50],number[50], email[50];
     };
-struct contact contacts[123];
+    // temporary contacts for testing
+struct contact contacts[123] = {{"lioubi arabi", "+212600000000", "lioubi@gmail.com"},
+{"oussama teammate", "+212600000000", "oussama@gmail.com"}};
 struct contact add() {
     char newName[50], newNumber[50], newEmail[50];
     struct contact newContact;
@@ -29,6 +31,30 @@ struct contact add() {
     return newContact;
 }
 
+int show() {
+    
+    printf("%-20s%-20s%-30s\n", "NAME", "NUMBER", "EMAIL");
+    printf("-------------------------------------------------------------------\n");
+    if(n==0){
+        printf("you don't have any contacts\n\n");
+        return 0;
+    }
+    for(int i = 0; i<n; i++){
+        printf("%-20s%-20s%-30s\n", contacts[i].name, contacts[i].number, contacts[i].email);
+    }
+    printf("\n");
+    return 1;
+} 
+void search(){
+    char search[50];
+    printf("enter the search: ");
+    getchar();
+    fgets(search, 50, stdin);
+    for(int i = 0; i<n; i++){
+        
+    }
+}
+
 int main() {
     int choose;
     while(1){
@@ -37,7 +63,7 @@ int main() {
         printf("2. search a contact\n");
         printf("3. modify a contact\n");
         printf("4. delete a contact\n");
-        printf("5.show all\n\n");
+        printf("5. show all\n\n");
         printf("choose from the list: ");
         scanf("%d", &choose);
         getchar();
@@ -56,7 +82,7 @@ int main() {
             
             break;
             case 5:
-            
+                show();
             break;
             default: printf("choose a correct number!!!\n");
         }
