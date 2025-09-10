7c3e105f8ca2c67aa47cdb2ead486b4cc27c8a47
lioubi arabi <l.loubi1886@uca.ac.ma>
2025-09-10 09:39:44 +0100

    settings the dashboard

--- Final_project/main.c
+++ Final_project/main.c
@@ -2,11 +2,29 @@
 
 struct player {
     int id, numero,age, buts;
-    char fullname[50], poste[50], ;
+    char nom[50],prenom[50], poste[50] ;
+    };
     
-}
+struct player team[25];
 
 int main() {
+    int choose;
+    while(choose!=7){
+        printf("--- dashboard ---\n");
+        printf("1. Ajouter un joueur\n");
+        printf("1. Ajouter un joueur\n");
+        printf("1. Ajouter un joueur\n");
+        printf("1. Ajouter un joueur\n");
+        printf("1. Ajouter un joueur\n");
+        printf("1. Ajouter un joueur\n");
+        printf("7. Quitter\n");
+        
+        printf("Choisissez un numéro: ");
+        scanf("%d",&choose);
+        
+    }
+    
+    
     
     return 0;
 }
